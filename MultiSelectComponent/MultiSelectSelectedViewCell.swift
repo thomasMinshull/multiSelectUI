60f49ccd9a1147b38e0eb338eaf7090490b0d4e5
ThomasMinshull <thomasminshull@gmail.com>
2017-05-16 14:04:54 -0700

    Implement MultiSelectSelectedViewCell (abstract class) and implement subclass MultiSelectSelectedViewCellWithButton, also removed more dead commented out code

---
+++ MultiSelectComponent/MultiSelectSelectedViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  MultiSelectSelectedViewCell.swift
+//  MultiSelectComponent
+//
+//  Created by ThomasMinshull on 2017-05-16.
+//  Copyright © 2017 DrBill. All rights reserved.
+//
+
+import UIKit
+
+class MultiSelectSelectedViewCell: UICollectionViewCell {
+    var textLabel: UILabel! {
+        get {
+            return self.textLabel
+        }
+        
+        set {
+            self.textLabel = newValue
+        }
+    }
+    
+    var detailTextLabel: UILabel! {
+        get {
+            return self.detailTextLabel
+        }
+        
+        set {
+            self.detailTextLabel = newValue
+        }
+    }
+    
+}
